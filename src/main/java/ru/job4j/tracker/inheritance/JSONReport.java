2b78280a867a81f35a5f59508c4051fd184ffc13
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-08-20 14:35:01 +0300

    4. Переопределение [#335575]

--- src/main/java/ru/job4j/tracker/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/tracker/inheritance/JSONReport.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker.inheritance;
 public class JSONReport extends TextReport {
 
     public String generate(String name, String body){
-        return name + " : name" + System.lineSeparator()+ body + " : body";
+        char quotes = '"';
+        return (quotes + name + quotes + " : \"name\"" + System.lineSeparator() + quotes + body + quotes+ " : \"body\"");
     }
 }
