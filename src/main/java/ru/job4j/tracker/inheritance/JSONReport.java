0a504b01088df2a45ada3312ba6bed1125d05ed8
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-08-20 14:47:59 +0300

    5. Аннотация @Override [#335576]

--- src/main/java/ru/job4j/tracker/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/tracker/inheritance/JSONReport.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker.inheritance;
 
 public class JSONReport extends TextReport {
 
+    @Override
     public String generate(String name, String body){
         char quotes = '"';
         return (quotes + name + quotes + " : \"name\"" + System.lineSeparator() + quotes + body + quotes+ " : \"body\"");
