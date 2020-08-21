1ac7f60508fe87577d682d19747eccf7fc9a464c
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-08-21 16:52:05 +0300

    5. Tracker - хранилище [#335590]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,7 +3,9 @@ package ru.job4j.tracker;
 public class StartUI {
 
     public static void main(String[] args) {
-        Item item = new Item();
-        System.out.println(item);
+        Item item = new Item(1, "Yaroslav");
+       Tracker tracker = new Tracker();
+       tracker.add(item);
+        System.out.println(tracker.findById(1));
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,34 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll(){
+        Item[] namesWithoutNull = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item name = items[index];
+            if (name != null) {
+                namesWithoutNull[size] = name;
+                size++;
+            }
+        }
+        namesWithoutNull = Arrays.copyOf(namesWithoutNull, size);
+        return namesWithoutNull;
+    }
+
+    public Item[] findByName(String key){
+        Item[] equalName = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item name = items[index];
+            if (name.getName().equals(key)) {
+                equalName[size] = name;
+                size++;
+            }
+        }
+        equalName = Arrays.copyOf(equalName, size);
+        return equalName;
     }
 
     public Item findById(int id) {
