97e3a65b577880551a92c3195109d3f80efd83f9
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-08-24 15:25:46 +0300

    8. Реализация меню за счет шаблона стратегия. [#335517]

--- src/main/java/ru/job4j/tracker/ShowAction.java
+++ src/main/java/ru/job4j/tracker/ShowAction.java
@@ -3,7 +3,7 @@ package ru.job4j.tracker;
 public class ShowAction implements UserAction{
     @Override
     public String name() {
-        return null;
+        return "Found All Items";
     }
 
     @Override
