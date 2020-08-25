35fc3bfa2e2416e9678d2e4a208cccdc7e9d8677
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-08-25 17:00:19 +0300

    1. Телефонный справочник на базе ArrayList [#335488]

---
+++ src/main/java/ru/job4j/tracker/search/PhoneDictionary.java
@@ -0,0 +1,26 @@
+package ru.job4j.tracker.search;
+
+import java.util.ArrayList;
+
+    public class PhoneDictionary {
+        private ArrayList<Person> persons = new ArrayList<Person>();
+
+        public void add(Person person) {
+            this.persons.add(person);
+        }
+
+        public ArrayList<Person> find(String key) {
+            ArrayList<Person> result = new ArrayList<>();
+
+            for (Person person : persons) {
+                if (key.contains(person.getAddress())
+                        ||key.contains(person.getName())
+                        ||key.contains(person.getSurname())
+                        ||key.contains(person.getPhone())){
+                    result.add(person);
+                };
+            }
+            return result;
+        }
+    }
+
