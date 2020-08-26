1221cc5e6ce2311c69ed83d5e24bd1e0c6298451
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-08-26 18:21:13 +0300

    9.1 Зависимость от System.out [#335507]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,6 +6,11 @@ import java.sql.SQLOutput;
 
 
 public class StartUI {
+    private final Output out;
+
+    public StartUI(Output out) {
+        this.out = out;
+    }
 
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
@@ -16,24 +21,28 @@ public class StartUI {
             run = action.execute(input, tracker);
         }
     }
+
     private void showMenu(UserAction[] actions) {
-        System.out.println("Menu.");
+        out.println("Menu.");
         for (int index = 0; index < actions.length; index++) {
-            System.out.println(index + ". " + actions[index].name());
+            out.println(index + ". " + actions[index].name());
         }
     }
-            public static void main (String[]args){
-                Input input = new ConsoleInput();
-                Tracker tracker = new Tracker();
-                UserAction[] actions = {
-                        new CreateAction(),
-                        new ShowAction(),
-                        new DeleteAction(),
-                        new FindByIdAction(),
-                        new ReplaceAction(),
-                        new FindByNameAction(),
-                        new Exit()
-                };
-                new StartUI().init(input, tracker, actions);
-            }
+
+    public static void main(String[] args) {
+        Output output = new ConsoleOutput();
+        Input input = new ConsoleInput();
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(output),
+                new ShowAction(),
+                new ReplaceAction(),
+                new DeleteAction(),
+                new FindByIdAction(),
+                new FindByNameAction(),
+                new Exit(),
+
+        };
+        new StartUI(output).init(input, tracker, actions);
+    }
 }
--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -29,49 +29,6 @@ public class StartUITest {
 //        assertThat(replaced.getName(), is("replaced item"));
 //    }
 //
-    @Test
-    public void whenCreateItem() {
-        Input in = new StubInput(
-                new String[]{"0", "Item name", "1"}
-        );
-        Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new CreateAction(),
-                new Exit()
-        };
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findAll()[0].getName(), is("Item name"));
-    }
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = tracker.add(new Item("Deleted item"));
-        Input in = new StubInput(
-                new String[] {"0", "1","1"}
-        );
-        UserAction[] actions = {
-                new DeleteAction(),
-                new Exit()
-        };
-        new StartUI().init(in, tracker, actions);
-        Item nullValue = null;
-        assertThat(tracker.findById(item.getId()), is(nullValue)); //тут сомнения.
-    }
-    @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = tracker.add(new Item("Replaced item"));
-        String replacedName = "New item name";
-        Input in = new StubInput(
-                new String[] {"0", replacedName, "1", "1"}
-        );
-        UserAction[] actions = {
-                new ReplaceAction(),
-                new Exit()
-        };
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
-    }
-}
 
+}
 
