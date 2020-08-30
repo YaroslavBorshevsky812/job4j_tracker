adf9c3e63d3b48b004f5698059d1f3f768e06840
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-08-30 18:32:17 +0300

    2. Идентичные тексты [#335502]

---
+++ src/main/java/ru/job4j/tracker/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package ru.job4j.tracker.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = false;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String words:origin) {
+            check.add(words);
+        }
+        for (String words:check) {
+            if(duplicateText.contains(words)){
+                rsl = true;
+            }
+            }
+        return rsl;
+    }
+}
