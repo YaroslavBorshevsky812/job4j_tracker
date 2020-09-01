a3ef533b0b03574047d962f2d2c3e23cfacc795f
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-09-01 14:10:45 +0300

    0. Map, HashMap [#335496]

---
+++ src/main/java/ru/job4j/tracker/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Speransky812@mail.ru", "Borshevsky Yaroslav");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
