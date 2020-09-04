09fd53c2ebe60a67138fcb118fa808a450f5a9ed
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-09-04 17:24:19 +0300

    9.2. Тесты вывода на консоль [#335508]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -5,30 +5,35 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class StartUITest {
-    //    @Test
-//    public void whenAddItem() {
-//        String[] answers = {"Fix PC"};
-//        Input input = new StubInput(answers);
-//        Tracker tracker = new Tracker();
-//        StartUI.createItem(input, tracker);
-//        Item created = tracker.findAll()[0];
-//        Item expected = new Item("Fix PC");
-//        assertThat(created.getName(), is(expected.getName()));
-//    }
-//    @Test
-//    public void whenReplaceItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = new Item("new item");
-//        tracker.add(item);
-//        String[] answers = {
-//                String.valueOf(item.getId()), // id сохраненной заявки в объект tracker.
-//                "replaced item"
-//        };
-//        StartUI.replaceItem(new StubInput(answers), tracker);
-//        Item replaced = tracker.findById(item.getId());
-//        assertThat(replaced.getName(), is("replaced item"));
-//    }
-//
-
+    @Test
+    public void whenExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new Exit()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is("Menu." + System.lineSeparator() +
+                        "0. === Exit ===" + System.lineSeparator()
+        ));
+    }
+    @Test
+    public void whenCreate() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is("Menu." + System.lineSeparator() +
+                "0. === Create a new Item ====" + System.lineSeparator()
+        ));
+    }
 }
 
