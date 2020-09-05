a0e9d680208eda831c532e98490d4280148c1580
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-09-05 22:07:11 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#335529]

--- src/main/java/ru/job4j/tracker/ex/Factorial.java
+++ src/main/java/ru/job4j/tracker/ex/Factorial.java
@@ -7,11 +7,11 @@ public class Factorial {
 
     public int calc(int n) {
         int rsl = 1;
+        if (n<0){
+            throw new  IllegalArgumentException("n<0");
+        }
         for (int index = 1; index <= n; index++) {
             rsl *= index;
-            if (n<0){
-                throw new  IllegalArgumentException("n<0");
-            }
         }
         return rsl;
     }
