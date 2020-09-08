6863922fb232639a090a6fbf2a72e1639706b0a6
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-09-08 15:41:27 +0300

    2. Singleton. [#335563]

--- src/main/java/ru/job4j/tracker/TrackerSingleOne.java
+++ src/main/java/ru/job4j/tracker/TrackerSingleOne.java
@@ -1,19 +1,18 @@
 package ru.job4j.tracker;
 
 public class TrackerSingleOne {
+
+    private TrackerSingleOne tracker = new TrackerSingleOne().getInstance().tracker;
+
     private TrackerSingleOne() {
     }
 
     public static TrackerSingleOne getInstance() {
-        return Holder.INSTANCE;
+        return new TrackerSingleOne();
     }
 
     public Item add(Item model) {
         return model;
-    }
-
-    private static final class Holder {
-        private static final TrackerSingleOne INSTANCE = new TrackerSingleOne();
     }
 }
 
