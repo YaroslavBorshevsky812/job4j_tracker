e696e245f438917674d28157b78678affc9119d1
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-09-10 19:39:41 +0300

    3. Банковские переводы [#335498]

--- src/main/java/ru/job4j/tracker/bank/BankService.java
+++ src/main/java/ru/job4j/tracker/bank/BankService.java
@@ -1,26 +1,43 @@
 package ru.job4j.tracker.bank;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BankService {
     private Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
-
+        users.put(user, new ArrayList<>());
     }
 
     public void addAccount(String passport, Account account) {
+        for (Account count:users.get(findByPassport(passport))) {
+            if(count.equals(account)){
+                System.out.println("Такой аккаунт уже есть");
+            }
 
+
+
+        }
     }
 
     public User findByPassport(String passport) {
-        return null;
+        User some = new User(passport, "buf");
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)){
+                some = user;
+            }
+        }
+        return some;
     }
 
     public Account findByRequisite(String passport, String requisite) {
-        return null;
+        Account account = new Account(" ", 0);
+        for (Account buf : users.get(findByPassport(passport))) {
+            if(buf.getRequisite().equals(requisite)){
+                account = buf;
+            }
+        }
+        return account;
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
