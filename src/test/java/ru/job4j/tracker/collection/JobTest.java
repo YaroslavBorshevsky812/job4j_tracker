c10eece7ea3a7195d6e70c560f114af5df61607c
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-09-12 13:21:24 +0300

    2. Комбинированный компаратор [#335485]

--- src/test/java/ru/job4j/tracker/collection/JobTest.java
+++ src/test/java/ru/job4j/tracker/collection/JobTest.java
@@ -7,12 +7,31 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 public class JobTest {
+
     @Test
     public void whenCompatorByNameAndPrority() {
         Comparator<Job> cmpNamePriority = new JobNameCompare().thenComparing(new JobPriorityCompare());
         int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("Petr", 0),
+                new Job("Petr", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+    @Test
+    public void whenCompatorByNameAndProrityReverse() {
+        Comparator<Job> cmpNamePriority = new JobNameCompareReverse().thenComparing(new JobPriorityCompareReverse());
+        int rsl = cmpNamePriority.compare(
+                new Job("Petr", 0),
+                new Job("Ivan", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+    @Test
+    public void whenCompatorByNameAndProrityThenFirstMore() {
+        Comparator<Job> cmpNamePriority = new JobNameCompare().thenComparing(new JobPriorityCompare());
+        int rsl = cmpNamePriority.compare(
+                new Job("Petr", 0),
+                new Job("Ivan", 1)
         );
         assertThat(rsl, greaterThan(0));
     }
