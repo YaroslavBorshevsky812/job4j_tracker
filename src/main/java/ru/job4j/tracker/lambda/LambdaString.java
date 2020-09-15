8e777c5b856798a3ef278b03062c0f6dd0908ce6
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-09-15 12:37:05 +0300

    2.2. Лямбда блок [#335318]

--- src/main/java/ru/job4j/tracker/lambda/LambdaString.java
+++ src/main/java/ru/job4j/tracker/lambda/LambdaString.java
@@ -4,14 +4,16 @@ import java.util.Comparator;
 
 public class LambdaString {
 
-    Comparator<Attachment> comparator = (left, right) -> {
-        System.out.println("compare - " + left.getSize() + " : " + right.getSize());
-        return left.getSize() - right.getSize();
-    };
+    public static void main(String[] args) {
 
-    Comparator<Attachment> comparatorStrings = (left, right) -> {
-        System.out.println("compare - " + " " + left.getName() + " " + right.getName());
-        return right.getName().compareTo(left.getName());
-    };
+        Comparator<Attachment> comparator = (left, right) -> {
+            System.out.println("compare - " + left.getSize() + " : " + right.getSize());
+            return left.getSize() - right.getSize();
+        };
 
+        Comparator<Attachment> comparatorStrings = (left, right) -> {
+            System.out.println("compare - " + " " + left.getName() + " " + right.getName());
+            return right.getName().compareTo(left.getName());
+        };
+    }
 }
