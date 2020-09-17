2e912beafbe2b99dab4eb827a98d7bf32095c809
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-09-17 13:15:03 +0300

    1. Фильтрация учеников [#335326]

--- src/main/java/ru/job4j/tracker/stream/School.java
+++ src/main/java/ru/job4j/tracker/stream/School.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 
 public class School {
     public static List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        List<Student> newList = students.stream().collect(Collectors.toList());
+        List<Student> newList = students.stream().filter(predict).collect(Collectors.toList());
         return newList;
     }
 }
--- src/test/java/ru/job4j/tracker/stream/SchoolTest.java
+++ src/test/java/ru/job4j/tracker/stream/SchoolTest.java
@@ -8,22 +8,66 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasItem;
 import static org.junit.Assert.*;
 
 public class SchoolTest {
 
+    @Test
     public void classA(){
+        List<Student> list = new ArrayList<>();
+        Student student = new Student("Surname", 77);
+        Student student1 = new Student("Surname", 69);
         Predicate<Student> predict = new Predicate<Student>() {
             @Override
             public boolean test(Student student) {
-               boolean rsl = true;
-                if (student.getScore() <= 70 || student.getScore() >= 100){
+                boolean rsl = false;
+                if(student.getScore() >=70 || student.getScore() <= 100){
                     rsl = true;
                 }
                 return rsl;
             }
         };
+        list.stream().collect(list, predict.test());
+
+        assertThat(list, hasItem(student));
+    }
+    @Test
+    public void classB(){
         List<Student> list = new ArrayList<>();
-        list.stream().collect(list, predict.test(student));
+        Student student = new Student("Surname", 51);
+        Student student1 = new Student("Surname", 69);
+        Predicate<Student> predict = new Predicate<Student>() {
+            @Override
+            public boolean test(Student student) {
+                boolean rsl = false;
+                if(student.getScore() >= 50 || student.getScore() < 70){
+                    rsl = true;
+                }
+                return rsl;
+            }
+        };
+        list.stream().collect(list, predict.test());
+
+        assertThat(list, hasItem(student));
+    }
+    @Test
+    public void classB(){
+        List<Student> list = new ArrayList<>();
+        Student student = new Student("Surname", 2);
+        Student student1 = new Student("Surname", 69);
+        Predicate<Student> predict = new Predicate<Student>() {
+            @Override
+            public boolean test(Student student) {
+                boolean rsl = false;
+                if(student.getScore() > 0 || student.getScore() < 50){
+                    rsl = true;
+                }
+                return rsl;
+            }
+        };
+        list.stream().collect(list, predict.test());
+
+        assertThat(list, hasItem(student));
     }
 }
