bf380b4959221e9a9fabc50a1ad3aa8ff9b93c0d
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-09-17 17:42:42 +0300

    1. Фильтрация учеников [#335326]

--- src/test/java/ru/job4j/tracker/stream/SchoolTest.java
+++ src/test/java/ru/job4j/tracker/stream/SchoolTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -16,58 +17,26 @@ public class SchoolTest {
     @Test
     public void classA(){
         List<Student> list = new ArrayList<>();
+        List<Student> resultList = new ArrayList<>();
         Student student = new Student("Surname", 77);
         Student student1 = new Student("Surname", 69);
-        Predicate<Student> predict = new Predicate<Student>() {
-            @Override
-            public boolean test(Student student) {
-                boolean rsl = false;
-                if(student.getScore() >=70 || student.getScore() <= 100){
-                    rsl = true;
-                }
-                return rsl;
-            }
-        };
-        list.stream().collect(list, predict.test());
+        Predicate<Student> predict = student2 -> student.getScore() > 0 || student.getScore() <50;
+        resultList.stream().collect(list, predict);
+
 
         assertThat(list, hasItem(student));
     }
-    @Test
-    public void classB(){
-        List<Student> list = new ArrayList<>();
-        Student student = new Student("Surname", 51);
-        Student student1 = new Student("Surname", 69);
-        Predicate<Student> predict = new Predicate<Student>() {
-            @Override
-            public boolean test(Student student) {
-                boolean rsl = false;
-                if(student.getScore() >= 50 || student.getScore() < 70){
-                    rsl = true;
-                }
-                return rsl;
-            }
-        };
-        list.stream().collect(list, predict.test());
 
-        assertThat(list, hasItem(student));
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(((Student) o).getScore(), student.getScore());
     }
-    @Test
-    public void classB(){
-        List<Student> list = new ArrayList<>();
-        Student student = new Student("Surname", 2);
-        Student student1 = new Student("Surname", 69);
-        Predicate<Student> predict = new Predicate<Student>() {
-            @Override
-            public boolean test(Student student) {
-                boolean rsl = false;
-                if(student.getScore() > 0 || student.getScore() < 50){
-                    rsl = true;
-                }
-                return rsl;
-            }
-        };
-        list.stream().collect(list, predict.test());
 
-        assertThat(list, hasItem(student));
+    @Override
+    public int hashCode() {
+        return Objects.hash();
     }
 }
