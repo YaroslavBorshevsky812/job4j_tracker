4ca0345f7ffd6aaa1f40c88737f5148843f9bfad
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-09-20 12:06:39 +0300

    0. Stream API улучшения [#335336]

--- src/main/java/ru/job4j/tracker/stream/StudentLevel.java
+++ src/main/java/ru/job4j/tracker/stream/StudentLevel.java
@@ -2,16 +2,17 @@ package ru.job4j.tracker.stream;
 
 import java.util.List;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-//public class StudentLevel {
-//    public static List<Student> levelOf(List<Student> students, int bound) {
-//        return students.stream()
-//                .filter(st -> st.getScore() > bound)
-//                .filter(student -> Stream.ofNullable(student)
-//                .sorted((left, right) -> left.getScore().compareTo(right.getScore()))
-//                .takeWhile(st -> (!st.equals(null)) )
-//                .collect(Collectors.toList());
-//    }
-//}
+public class StudentLevel {
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .filter(Objects::nonNull)
+                .sorted((left, right) -> Integer.compare(right.getScore(), left.getScore()))
+                .takeWhile(st -> st.getScore() > bound)
+                .collect(Collectors.toList());
+    }
+}
+
