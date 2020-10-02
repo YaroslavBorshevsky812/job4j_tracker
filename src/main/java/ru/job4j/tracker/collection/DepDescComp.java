373d603456714110156967466c59e9dc57edddf0
YaroslavBorshevsky812 <Speransky812@mail.ru>
2020-10-02 09:51:13 +0300

    2. Отсортировать департаменты [#335478]

--- src/main/java/ru/job4j/tracker/collection/DepDescComp.java
+++ src/main/java/ru/job4j/tracker/collection/DepDescComp.java
@@ -5,6 +5,12 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        return o1.compareTo(o2);
+        String [] buf = o1.split("/");
+        String [] buf1 = o2.split("/");
+        int rsl = buf1[0].compareTo(buf[0]);
+            if(rsl == 0){
+                return o1.compareTo(o2);
+            }
+        return rsl;
     }
 }
